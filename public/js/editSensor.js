4a34766af01261aeff441df2c5745f49ab2e8013
Kornel Stępnik <Kornel Stpnik>
2016-02-21 01:13:08 +0100

    Edit sensor works fine now

--- public/js/editSensor.js
+++ public/js/editSensor.js
@@ -1,48 +1,22 @@
 /**
  * funckje podstrony do edycji sensora
  */
+'use strict'
+
+var sensorID;
+var selectedSensor = {}
+var $selectedItem;
+
 $(document).ready(function () {
     console.log('zaladowano podstrone sensor')
-    console.log('sensorID: ' + sensorID)
-
-    // $(document).on('click', 'div#sensorListGroup a', function (e) {
-    //     e.preventDefault();
-    //     $selectedItem = $(this);
-    //     $(this).addClass('list-group-item active').siblings()
-    //         .removeClass('list-group-item active')
-    //         .addClass('list-group-item');
-        
-    //     //ustaw wartosc sensorID na wybrana po kliknieciu
-    //     sensorID = $(this).attr('data-sensid');
-        
-    //     //wyswietl zakladke do edycji sensora
-    //     $('#editSensorTab').show();
-
-    //     getSensorByID(sensorID, function (result) {
-    //         //ustaw objekt sensora
-    //         selectedSensor = result
-        
-    //         //ustaw opis sensora w polu tekstowym
-    //         $('#inpuDescEditSens').val(selectedSensor.desc)
-        
-    //         //ustaw wartosc checkboxa favourite
-    //         if (selectedSensor.favourite === 1) {
-    //             $('#editSensor fieldset input#favCheckBox').prop('checked', true)
-    //         } else {
-    //             $('#editSensor fieldset input#favCheckBox').prop('checked', false)
-    //         }
-    //         //ustaw wartosc checkboxa stan
-    //         if (selectedSensor.state === 1) {
-    //             $('#editSensor fieldset input#stateCheckBox').prop('checked', true)
-    //         } else {
-    //             $('#editSensor fieldset input#stateCheckBox').prop('checked', false)
-    //         }
-    //     })
-    // })
     
+    //rejestruj klikniecia w liscie sensorow
     $(document).on('click', 'div#sensorListGroup a', function (e) {
         e.preventDefault();
+        
+        //wybrany objekt DOM 
         $selectedItem = $(this);
+        
         //ustaw wartosc sensorID na wybrana po kliknieciu
         sensorID = $(this).attr('data-sensid');
         console.log('sensorID: ' + sensorID);
@@ -73,23 +47,12 @@ $(document).ready(function () {
     $(document).on('click', '.inpuDescEditSens', function () {
         $(this).val('');
     });
+    
+    //rejestruj klikniecia checkbox'ow i przycisku zmiany edycji
     $(document).on('click', 'input.favCheckBox', checkFavourite);
     $(document).on('click', 'input.stateCheckBox', checkState);
     $(document).on('click', '.editSensorButton', editSensor);
-    //$('input.favCheckBox').click(checkFavourite);
-    //$('input.stateCheckBox').click(checkState);
-    //$('.editSensorButton').on('click', editSensor);
 });
-
-
-var sensorID;
-
-var selectedSensor = {}
-var $selectedItem;
-
-console.log('zaladowano podstrone edit sensor')
-
-//DRUGA WERSJA + callback
 
 function getSensorByID(id, callback) {
     console.log('/api/sensor/' + id)
@@ -99,14 +62,14 @@ function getSensorByID(id, callback) {
 }
 //sprawdz stan przelacznika stan i aktualizuj obiekt selectedSensor
 var checkState = function () {
-    var $stateCheckbox = $('input#stateCheckBox:checked');
+    var $stateCheckbox = $('input.stateCheckBox:checked');
     selectedSensor.state = $stateCheckbox.length;
     console.log('stan state: ' + $stateCheckbox.length);
 }
 
 //sprawdz stan przelacznika ulubiony i aktualizuj obiekt selectedSensor
 var checkFavourite = function () {
-    var $favCheckbox = $('input#favCheckBox:checked');
+    var $favCheckbox = $('input.favCheckBox:checked');
     selectedSensor.favourite = $favCheckbox.length;
     console.log('stan fav: ' + $favCheckbox.length);
 }
@@ -115,21 +78,22 @@ var checkFavourite = function () {
 function editSensor(event) {
     event.preventDefault();
     var errorCount = 0;
-    console.log($(this));
-    // $('.editSensor input').each(function (index, val) {
-    //     console.log(val);
-    //     if ($(this).val() === '') {
-    //         errorCount++;
-    //     }
-    // });
+    
+    //znajdz rodzica przycisku a dla niego znajdz wszystkie pola tekstowe
+    var $parent = $(this).parent().find('input[type=text]');
+    
+    //walidacja wszystkich pol tekstowych
+    $parent.each(function (index, val) {
+        if ($(this).val() === '') {
+            errorCount++;
+        } else {
+           selectedSensor.desc =  $(this).val()
+        }
+    });
     console.log(errorCount)
 
     if (errorCount === 0) {
-        selectedSensor.desc = $('.inpuDescEditSens').val()
-
-        console.log(selectedSensor);
-        console.log('sensor id: ' + sensorID);
-
+        console.log('zmieniony senosr: ' + selectedSensor);
         $.ajax({
             type: 'PUT',
             data: selectedSensor,
@@ -139,8 +103,7 @@ function editSensor(event) {
             console.log(response)
 
             if (response.error === false) {
-                // $('#addHub fieldset input').val('');
-                // populateTableHubs();
+                //populateTableSensors();
                 $selectedItem.text(selectedSensor.sensorID + ' ' + selectedSensor.desc);
                 alert('Zmieniono ustawienia');
             } else {
